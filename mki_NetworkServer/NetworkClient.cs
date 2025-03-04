1e1b04aa91f7caed2ce67181800239429e7e7d2f
mki821 <alexalex0821@naver.com>
2025-03-04 18:47:48 +0900

    Init

---
+++ mki_NetworkServer/NetworkClient.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace mki_NetworkServer
+{
+    class NetworkClient
+    {
+        public int id;
+        public TcpClient client;
+        public NetworkStream stream;
+        public Thread thread;
+        public int curRoomID = -1;
+    }
+
+    class Box
+    {
+        public string command;
+        public object data;
+
+        public Box(string command, object data)
+        {
+            this.command = command;
+            this.data = data;
+        }
+
+        public string ToJson() => JsonConvert.SerializeObject(this);
+    }
+
+    [Serializable]
+    class Basic
+    {
+        public object obj1;
+        public object obj2;
+
+        public Basic(object obj1, object obj2)
+        {
+            this.obj1 = obj1;
+            this.obj2 = obj2;
+        }
+    }
+
+    [Serializable]
+    class RoomInfo
+    {
+        public int roomID;
+        public string roomName;
+        public int personnel;
+
+        public RoomInfo(int id, string name, int personnel)
+        {
+            roomID = id;
+            roomName = name;
+            this.personnel = personnel;
+        }
+    }
+
+    class Room
+    {
+        private static int id = 0;
+
+        public int roomID;
+        public string roomName;
+        public NetworkClient host;
+        public NetworkClient[] clients = new NetworkClient[2];
+
+        public Room(NetworkClient host, string roomName)
+        {
+            roomID = id++;
+            this.host = host;
+            this.roomName = roomName;
+            clients[0] = host;
+
+            byte[] buffer = Encoding.UTF8.GetBytes($"{new Box("success.room", true).ToJson()}\\ENDBUFFER\\");
+            host.client.GetStream().Write(buffer, 0, buffer.Length);
+            SetTeam(host.client, 0);
+        }
+
+        public bool TryConnect(NetworkClient client)
+        {
+            Check();
+            if (clients[0] == null && clients[1] == null) return false;
+
+            if (clients[0] == null)
+            {
+                clients[0] = client;
+                SetTeam(client.client, 0);
+                return true;
+            }
+            else if (clients[1] == null)
+            {
+                clients[1] = client;
+                SetTeam(client.client, 1);
+                return true;
+            }
+            return false;
+        }
+
+        public int GetPersonnel()
+        {
+            Check();
+            int count = 0;
+            if (clients[0] != null)
+                count++;
+            if (clients[1] != null)
+                count++;
+
+            return count;
+        }
+
+        public void BroadCast(byte[] buffer, int size)
+        {
+            Check();
+            clients[0]?.client.GetStream().Write(buffer, 0, size);
+            clients[1]?.client.GetStream().Write(buffer, 0, size);
+        }
+
+        public void Swap()
+        {
+            Check();
+            (clients[0], clients[1]) = (clients[1], clients[0]);
+
+            if(clients[0] != null)
+                SetTeam(clients[0].client, 0);
+            if(clients[1] != null)
+                SetTeam(clients[1].client, 1);
+        }
+
+        public void Delete()
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes($"{new Box("disconnect.room", 0).ToJson()}\\ENDBUFFER\\");
+
+            BroadCast(buffer, buffer.Length);
+        }
+
+        private void Check()
+        {
+            if (clients[0] != null)
+                if (!clients[0].client.Connected)
+                {
+                    clients[0] = null;
+                }
+            if (clients[1] != null)
+                if (!clients[1].client.Connected)
+                {
+                    clients[1] = null;
+                }
+        }
+
+        public void SetTeam(TcpClient tc, int team)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes($"{new Box("set.team", team).ToJson()}\\ENDBUFFER\\");
+            tc.GetStream().Write(buffer, 0, buffer.Length);
+
+            buffer = Encoding.UTF8.GetBytes($"{new Box("wyn.lobby", null).ToJson()}\\ENDBUFFER\\");
+            tc.GetStream().Write(buffer, 0, buffer.Length);
+        }
+
+        public bool Exit(NetworkClient nc) {
+            Check();
+            if (clients[0] == nc)
+            {
+                clients[0] = null;
+            }
+            else if (clients[1] == nc)
+            {
+                clients[1] = null;
+            }
+
+            if(clients[0] == null && clients[1] == null)
+            {
+                Delete();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ mki_NetworkServer/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace mki_NetworkServer
+{
+    class Program
+    {
+        private static Dictionary<TcpClient, NetworkClient> clients = new Dictionary<TcpClient, NetworkClient>();
+        private static Dictionary<int, Room> rooms = new Dictionary<int, Room>();
+        private static Dictionary<int, RoomInfo> roomInfos = new Dictionary<int, RoomInfo>();
+        private static string address = @$"{Directory.GetCurrentDirectory()}/log.txt";
+
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(ListenerThread);
+            thread.Start();
+        }
+
+        private static void ListenerThread()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 5500);
+            int count = 0;
+
+            try
+            {
+                listener.Start();
+                while (true)
+                {
+                    int id = ++count;
+                    TcpClient tc = listener.AcceptTcpClient();
+
+                    Thread th = new Thread(() => Send(tc, id));
+                    th.Start();
+
+                    clients.Add(tc, new NetworkClient()
+                    {
+                        id = id,
+                        client = tc,
+                        stream = tc.GetStream(),
+                        thread = th
+                    });
+
+                    Console.WriteLine($"Conneted | ID: {id} / IP : {((IPEndPoint)tc.Client.RemoteEndPoint).Address}");
+                    Log($"Conneted | ID: {id} / IP : {((IPEndPoint)tc.Client.RemoteEndPoint).Address}");
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void Send(TcpClient tc, int id)
+        {
+            byte[] buffer = new byte[1024];
+            NetworkStream stream = tc.GetStream();
+
+            int bytes;
+            while ((bytes = stream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                if (!Encoding.UTF8.GetString(buffer).Contains("\\ENDBUFFER\\")) continue;
+
+                try
+                {
+                    Box box = JsonConvert.DeserializeObject<Box>(Encoding.UTF8.GetString(buffer).Replace("\\ENDBUFFER\\", ""));
+                    Log(Encoding.UTF8.GetString(buffer).Replace("\\ENDBUFFER\\", ""));
+
+                    if (box.command.Contains("room."))
+                        Room(box, tc);
+                    else
+                    {
+                        if(clients[tc].curRoomID != -1)
+                        {
+                            if(rooms[clients[tc].curRoomID] != null)
+                                rooms[clients[tc].curRoomID].BroadCast(buffer, bytes);
+                        }
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine(Encoding.UTF8.GetString(buffer));
+                }
+                Console.WriteLine($"[{DateTime.Now}] IP {((IPEndPoint)tc.Client.RemoteEndPoint).Address} / read {bytes}");
+                buffer = new byte[1024];
+            }
+            Console.WriteLine($"Disconneted | ID: {id} / IP : {((IPEndPoint)tc.Client.RemoteEndPoint).Address}");
+            Log($"Disconneted | ID: {id} / IP : {((IPEndPoint)tc.Client.RemoteEndPoint).Address}");
+            if (clients[tc].curRoomID != -1)
+            {
+                RemoveRoomInfo(clients[tc].curRoomID);
+            }
+
+            stream.Close();
+            tc.Close();
+        }
+
+        private static void Log(string content)
+        {
+            StreamWriter writer = new StreamWriter(address, true);
+
+            writer.WriteLine($"[{DateTime.Now}] {content}");
+            writer.Close();
+        }
+
+        private static void Room(Box box, TcpClient tc)
+        {
+            string command = box.command.Replace("room.", "");
+            NetworkClient nc = clients[tc];
+
+            switch(command)
+            {
+                case "make":
+                    MakeRoom(nc, box);
+                    break;
+                case "connect":
+                    ConnectRoom(tc, (int)(long)box.data);
+                    break;
+                case "exit":
+                    if (rooms[nc.curRoomID].Exit(nc)) RemoveRoomInfo(nc.curRoomID);
+                    break;
+                case "swap":
+                    rooms[nc.curRoomID].Swap();
+                    break;
+                case "delete":
+                    if(rooms[nc.curRoomID] != null)
+                    {
+                        rooms[nc.curRoomID].Delete();
+                        roomInfos.Remove(nc.curRoomID);
+                    }
+                    break;
+                case "refresh":
+                    RefreshRoom(tc);
+                    break;
+                case "curInfo":
+                    Send(tc, "info.room", new Basic(rooms[nc.curRoomID].GetPersonnel(), rooms[nc.curRoomID].roomName));
+                    break;
+            }
+        }
+
+        private static void MakeRoom(NetworkClient nc, Box box)
+        {
+            Room room = new Room(nc, box.data.ToString());
+            rooms[room.roomID] = room;
+            roomInfos[room.roomID] = (new RoomInfo(room.roomID, room.roomName, room.GetPersonnel()));
+
+            Console.WriteLine($"Create Room : {box.data}");
+
+            nc.curRoomID = room.roomID;
+        }
+
+        private static void ConnectRoom(TcpClient tc, int id)
+        {
+            if (rooms[id] == null) Send(tc, "try.room", "False"); ;
+
+            bool connect = rooms[id].TryConnect(clients[tc]);
+            if (connect)
+            {
+                clients[tc].curRoomID = rooms[id].roomID;
+            }
+            Send(tc, "try.room", connect.ToString());
+        }
+
+        private static void RefreshRoom(TcpClient tc)
+        {
+            foreach(RoomInfo r in roomInfos.Values)
+                if(r != null)
+                    r.personnel = rooms[r.roomID].GetPersonnel();
+
+            string roomsInfo = JsonConvert.SerializeObject(roomInfos.Values);
+            Send(tc, "refresh.room", roomsInfo);
+        }
+
+        private static void Send(TcpClient tc, string type, object data)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes($"{new Box(type, data).ToJson()}\\ENDBUFFER\\");
+            Log(new Box(type, data).ToJson());
+            tc.GetStream().Write(buffer, 0, buffer.Length);
+        }
+
+        public static void RemoveRoomInfo(int id)
+        {
+            foreach (RoomInfo r in roomInfos.Values)
+            {
+                if (r == null) continue;
+
+                if (r.roomID == id)
+                {
+                    roomInfos[id] = null;
+                    rooms[id]?.Delete();
+                    rooms[id] = null;
+                    break;
+                }
+            }
+        }
+    }
+}
